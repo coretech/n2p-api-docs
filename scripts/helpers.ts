8c42a3a8c36fcae7aaf1f9478571a632a0610164
Avi Charlop <avicharlop@gmail.com>
2021-05-04 13:27:53 -0500

    flatten webhooks refs

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,5 +1,8 @@
 import chalk from 'chalk';
+import path from 'path';
 import shell from 'shelljs';
+import yaml from 'js-yaml';
+import fs from 'fs'
 
 export function quit(err: string, param?: string) {
   const message = param ? `: ${param}` : '';
@@ -9,4 +12,33 @@ export function quit(err: string, param?: string) {
 
 export function print(str: string, color: string = 'blue') {
   shell.echo(chalk[color](str));
+}
+
+export const flattenAllOf = (allOf: Array<{ '$ref': string} | { properties: object}>, basePath: string) => {
+  let properties = {}
+
+  allOf.reverse().forEach(refObj => {
+    const ref = refObj['$ref']
+    if (ref) {
+      let parser: Function
+      if (ref.includes('yaml') || ref.includes('yml')) {
+        parser = yaml.load
+      } else if (ref.includes('json')) {
+        parser = JSON.parse
+      } else {
+        print('skipping' + ref)
+        return
+      }
+
+      const spec = parser(fs.readFileSync(path.join(basePath, ref), 'utf8'))
+      properties = Object.assign(spec['properties'], properties)
+    }
+  })
+
+  const originalProperties = allOf.filter(spec => spec['properties'])?.[0]?.['properties']
+  if (originalProperties) {
+    Object.assign(properties, originalProperties)
+  }
+
+  return properties
 }
